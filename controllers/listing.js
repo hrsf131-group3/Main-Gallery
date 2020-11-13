473b5614f87569ffeba6c4d34dc151e8ea218928
BrandonRussell2020 <brandon.e.russell4@gmail.com>
2020-11-13 00:07:26 -0800

    Controller File Added

---
+++ controllers/listing.js
@@ -0,0 +1,22 @@
+let Listing = require('../db/schema.js');
+
+getListings = (req, res) => {
+  Listing.getAllListings( (err, listings) => {
+   if (err) {
+      res.status(400).json({
+       success:false,
+       error: err
+     })
+   }
+     return res.status(200).json({
+       success: true,
+       data: listings
+     })
+     .catch(err => console.log(err))
+   }
+ )
+}
+
+module.exports = {
+  getListings
+}
