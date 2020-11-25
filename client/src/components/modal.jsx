27cac1b439c0800ec415f2d1f300db9afc418ed8
BrandonRussell2020 <brandon.e.russell4@gmail.com>
2020-11-24 22:31:44 -0800

    Updated Icons to point to s3

--- client/src/components/modal.jsx
+++ client/src/components/modal.jsx
@@ -93,11 +93,11 @@ class Modal extends React.Component {
             </button>
             <div className={styles['action-btns']}>
               <button className={styles['action-btn']}>
-                <img className={styles['action-icon']} src="./icons/heart-outline.png"/>
+                <img className={styles['action-icon']} src="https://s3-us-west-1.amazonaws.com/hackreactor.fec.trulia.photos/icons/heart-outline.png"/>
                 <p>Save</p>
               </button>
               <button className={styles['action-btn']}>
-                <img className={styles['action-icon']} src="./icons/share.png"/>
+                <img className={styles['action-icon']} src="https://s3-us-west-1.amazonaws.com/hackreactor.fec.trulia.photos/icons/share.png"/>
                 <p>Share</p>
               </button>
             <button className={styles['exit-x-btn']} onClick={this.props.handleClose}>
