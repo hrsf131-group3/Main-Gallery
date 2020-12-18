d7c205fd06d544f8d6c44b1738fc2b73c1f6c205
Chris <chrisldiep@gmail.com>
2020-12-17 21:38:45 -0800

    Changed pool size and removed data parsing

--- stress-testing/script.js
+++ stress-testing/script.js
@@ -8,17 +8,15 @@ export const options = {
   stages: [
     { duration: '30s', target: 400 },
     { duration: '1m', target: 400 },
+    { duration: '30s', target: 600 },
+    { duration: '1m', target: 600 },
     { duration: '30s', target: 800 },
     { duration: '1m', target: 800 },
-    { duration: '30s', target: 1200 },
-    { duration: '1m', target: 1200 },
-    { duration: '30s', target: 1600 },
-    { duration: '3m', target: 1600 },
-    { duration: '2m', target: 0 },
+    { duration: '1m', target: 0 },
   ],
 };
 export default function() {
-  const url = `/api/v1/homes/${Math.floor(Math.random() * 10000000) + 1}/`;
+  const url = `http://localhost:8040/api/v1/homes/${Math.floor(Math.random() * 10000000) + 1}/`;
 
   const res = http.get(url);
   const result = check(res, {
